a93fe011716d24af5c9440bed36564bad1c3aa69
Chanho Kim <nonamed19@naver.com>
2024-10-29 10:56:27 +0900

    241029

---
+++ Baekjoon/BJ_15650.py
@@ -0,0 +1,16 @@
+from itertools import permutations
+
+N, M = map(int, input().split())
+
+numbers = list(range(1, N+1))
+perms = list(permutations(numbers, M))
+
+results = []
+for perm in perms:
+    perm = tuple(sorted(perm))
+    results.append(perm)
+
+results = sorted(list(set(results)))
+
+for result in results:
+    print(*result)
