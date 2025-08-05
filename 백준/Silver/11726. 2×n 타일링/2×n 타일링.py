fe87424ecc59f8f8a5140dda5d96d22965180a2c
Chanho Kim <kchanho9773@gmail.com>
2025-08-05 19:23:06 +0900

    [Silver III] Title: 2×n 타일링, Time: 120 ms, Memory: 108384 KB -BaekjoonHub

---
+++ 백준/Silver/11726. 2×n 타일링/2×n 타일링.py
@@ -0,0 +1,22 @@
+# 2×n 크기의 직사각형을 1×2, 2×1 타일로 채우는 방법의 수를 구하는 프로그램을 작성하시오.
+# 첫째 줄에 2×n 크기의 직사각형을 채우는 방법의 수를 10,007로 나눈 나머지를 출력한다.
+
+import sys
+
+n = int(sys.stdin.readline())
+
+dp = [0]*(n+1)  # initialization
+
+for i in range(1, n+1):
+    # 규칙 찾기
+    if i == 1:
+        dp[1] = 1
+    elif i == 2:
+        dp[2] = 2
+    elif i == 3:
+        dp[3] = 3
+    else:
+        dp[i] = dp[i-1] + dp[i-2]
+
+result = dp[n] % 10007
+print(result)
