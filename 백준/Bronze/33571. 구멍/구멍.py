d0933ee47a0f3e271ca6cba4c377d08f41a94832
Chanho Kim <kchanho9773@gmail.com>
2025-08-20 09:11:14 +0900

    [Bronze IV] Title: 구멍, Time: 92 ms, Memory: 108384 KB -BaekjoonHub

---
+++ 백준/Bronze/33571. 구멍/구멍.py
@@ -0,0 +1,25 @@
+alphabet = {
+    'A': 1,
+    'a': 1,
+    'B': 2,
+    'b': 1,
+    'D': 1,
+    'd': 1,
+    'e': 1,
+    'g': 1,
+    'O': 1,
+    'o': 1,
+    'P': 1,
+    'p': 1,
+    'Q': 1,
+    'q': 1,
+    'R': 1,
+    '@': 1,
+}
+
+count = 0
+words = list(input())
+for word in words:
+    if word in alphabet:
+        count += alphabet[word]
+print(count)
