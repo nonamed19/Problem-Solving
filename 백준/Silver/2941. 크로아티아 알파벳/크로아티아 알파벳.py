dac73848963aefcb7a241276ea220ac75e4701df
Chanho Kim <kchanho9773@gmail.com>
2025-08-28 13:59:57 +0900

    [Silver V] Title: 크로아티아 알파벳, Time: 88 ms, Memory: 108384 KB -BaekjoonHub

--- 백준/Silver/2941. 크로아티아 알파벳/크로아티아 알파벳.py
+++ 백준/Silver/2941. 크로아티아 알파벳/크로아티아 알파벳.py
@@ -1,22 +1,13 @@
 import sys
 input = sys.stdin.readline
 
-def croatian(words):
-    alphabet = ['c=', 'c-', 'd-', 'lj', 'nj', 's=', 'z=']
-    if words == 'dz=':
-        return 3
-    elif words[:2] in alphabet:
-        return 2
-    else:
-        return 1
-
 
 words = input().strip()
 
-result, i = 0, 0
-while i <= len(words) - 1:
-    count = croatian(words[i:i+3])
-    i += count
-    result += 1
+alphabets = ['c=', 'c-', 'dz=', 'd-', 'lj', 'nj', 's=', 'z=']
 
-print(result)
+for alphabet in alphabets:
+    if alphabet in words:
+        words = words.replace(alphabet, '*')
+
+print(len(words))
