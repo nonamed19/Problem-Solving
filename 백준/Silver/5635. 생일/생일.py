676ccc58a17d4c717845cf5cadd45be2e64ec11e
Chanho Kim <kchanho9773@gmail.com>
2025-09-09 20:51:57 +0900

    [Silver V] Title: 생일, Time: 88 ms, Memory: 108384 KB -BaekjoonHub

--- 백준/Silver/5635. 생일/생일.py
+++ 백준/Silver/5635. 생일/생일.py
@@ -4,18 +4,11 @@ input = sys.stdin.readline
 
 n = int(input())  # 1 <= n <= 100
 
-names, dates = [], []
-for _ in range(n):
-    name, day, month, year = input().split()
-    if len(day) == 1:
-        day = '0' + day
-    if len(month) == 1:
-        month = '0' + month
-    names.append(name)
-    dates.append(year + month + day)
+students = [[name, int(month), int(day), int(year)] for name, month, day, year in (input().split() for _ in range(n))]
+students.sort(key=lambda x: (x[3], x[2], x[1]))
 
 # 가장 나이가 적은 사람의 이름
-print(names[dates.index(max(dates))])
+print(students[-1][0])
 
 # 가장 나이가 많은 사람 이름
-print(names[dates.index(min(dates))])
+print(students[0][0])
