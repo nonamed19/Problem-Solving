d22883108b54cbc2b8e5c872f01371f0eee050ee
Chanho Kim <kchanho9773@gmail.com>
2025-09-16 14:15:36 +0900

    [Bronze I] Title: 단어 퍼즐, Time: 960 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/9946. 단어 퍼즐/단어 퍼즐.py
+++ 백준/Bronze/9946. 단어 퍼즐/단어 퍼즐.py
@@ -1,22 +1,9 @@
 order = 0
 while True:
     order += 1
-    flag = True
     words1, words2 = input(), input()
 
     if words1 == 'END' and words2 == 'END':
         break
 
-    words1, words2 = sorted(words1), sorted(words2)
-
-    if len(words1) == len(words2):
-        for i in range(len(words1)):
-            if words1[i] != words2[i]:
-                flag = False
-    else:
-        flag = False
-
-    if flag:
-        print(f'Case {order}: same')
-    else:
-        print(f'Case {order}: different')
+    print(f'Case {order}: {'same' if sorted(words1) == sorted(words2) else 'different'}')
