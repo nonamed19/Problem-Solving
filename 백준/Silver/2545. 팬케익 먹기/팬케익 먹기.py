ced9a302876ffb03dbcbb9f970c74b35fa59b58b
Chanho Kim <kchanho9773@gmail.com>
2025-09-16 15:19:59 +0900

    [Silver IV] Title: 팬케익 먹기, Time: 48 ms, Memory: 32412 KB, Score: 3 point -BaekjoonHub

--- 백준/Silver/2545. 팬케익 먹기/팬케익 먹기.py
+++ 백준/Silver/2545. 팬케익 먹기/팬케익 먹기.py
@@ -1,18 +1,14 @@
 T = int(input())
 
 for _ in range(T):
-    _ = input()
+    input()
     A, B, C, D = map(int, input().split())
+    A, B, C = sorted((A, B, C))
 
-    for _ in range(D):
-        if A >= max(B, C):
-            A -= 1
-            continue
-        elif B >= max(A, C):
-            B -= 1
-            continue
-        else:
-            C -= 1
-            continue
+    S1 = A + B + C - D
+    A = min(A, S1//3)
+    S2 = S1 - A
+    B = min(B, S2//2)
+    C = S2 - B
 
     print(A*B*C)
