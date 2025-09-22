da10dccfb6c2bfa13ca7346b962d81a59db48d35
Chanho Kim <kchanho9773@gmail.com>
2025-09-22 13:57:04 +0900

    [Silver V] Title: 피터팬 프레임, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/3054. 피터팬 프레임/피터팬 프레임.py
@@ -0,0 +1,32 @@
+alphabets = str(input())
+
+results = [['.' for _ in range((4 * len(alphabets) + 1) if len(alphabets) > 1 else 5)] for _ in range(5)]
+
+# 피터팬 프레임
+for i in range(len(alphabets)):
+    if i == 0:
+        results[0][2] = '#'
+        results[1][1], results[1][3] = '#', '#'
+        results[2][0], results[2][2], results[2][4] = '#', alphabets[i], '#'
+        results[3][1], results[3][3] = '#', '#'
+        results[4][2] = '#'
+    elif (i+1)%3 == 0:
+        pass
+    else:
+        results[0][2 + i*4] = '#'
+        results[1][1 + i*4], results[1][3 + i*4] = '#', '#'
+        results[2][0 + i*4], results[2][2 + i*4], results[2][4 + i*4] = '#', alphabets[i], '#'
+        results[3][1 + i*4], results[3][3 + i*4] = '#', '#'
+        results[4][2 + i*4] = '#'
+
+# 3의 배수 위치, 웬디 프레임
+for i in range(len(alphabets)):
+    if (i+1)%3 == 0:
+        results[0][2 + i*4] = '*'
+        results[1][1 + i*4], results[1][3 + i*4] = '*', '*'
+        results[2][0 + i*4], results[2][2 + i*4], results[2][4 + i*4] = '*', alphabets[i], '*'
+        results[3][1 + i*4], results[3][3 + i*4] = '*', '*'
+        results[4][2 + i*4] = '*'
+
+for result in results:
+    print(*result, sep='')
