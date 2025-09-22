fd7ea16220c21c78578f57d6b0358bed718105d0
Chanho Kim <kchanho9773@gmail.com>
2025-09-22 14:12:05 +0900

    [Silver IV] Title: 문자열 집합, Time: 3756 ms, Memory: 37004 KB -BaekjoonHub

--- 백준/Silver/14425. 문자열 집합/문자열 집합.py
+++ 백준/Silver/14425. 문자열 집합/문자열 집합.py
@@ -2,10 +2,11 @@ import sys; input = sys.stdin.readline
 
 N, M = map(int, input().split())
 
-S = [input() for _ in range(N)]
+S = [input().strip() for _ in range(N)]
 
 result = 0
 for _ in range(M):
-    result += 1 if str(input()) in S else 0
+    if input().strip() in S:
+        result += 1
 
 print(result)
