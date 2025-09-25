d159488ac8be95be5fde9bef5879c673c0275148
Chanho Kim <kchanho9773@gmail.com>
2025-09-25 15:08:40 +0900

    [Bronze II] Title: 탕수육, Time: 44 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/13419. 탕수육/탕수육.py
+++ 백준/Bronze/13419. 탕수육/탕수육.py
@@ -2,14 +2,7 @@ T = int(input())
 
 for _ in range(T):
     phrases = input()
-    length = len(phrases)
+    phrases = phrases if len(phrases) % 2 == 0 else phrases*2
 
-    # 입력 데이터의 개수가 짝수인 경우
-    if length % 2 == 0:
-        print(*[phrases[2*i] for i in range(length//2)], sep='')
-        print(*[phrases[2*i+1] for i in range(length//2)], sep='')
-
-    # 입력 데이터의 개수가 짝수인 경우
-    else:  # elif length % 2 == 1:
-        print(*[phrases[2*i] for i in range(length//2+1)] + [phrases[2*i+1] for i in range(length//2)], sep='')
-        print(*[phrases[2*i+1] for i in range(length//2)] + [phrases[2*i] for i in range(length//2+1)], sep='')
+    print(phrases[::2])
+    print(phrases[1::2])
