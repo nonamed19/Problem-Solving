8ce82f0d81b468f76dcd6e62ce29e7e16b49de1e
Chanho Kim <kchanho9773@gmail.com>
2025-11-12 16:11:52 +0900

    [Bronze I] Title: 평균은 넘겠지, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/4344. 평균은 넘겠지/평균은 넘겠지.py
@@ -0,0 +1,9 @@
+C = int(input())
+
+average = 0
+for _ in range(C):
+    N, *scores = list(map(int, input().split()))
+    average = sum(scores) / N  # 평균 점수
+    above_average = sum(score > average for score in scores)  # 평균을 넘는 학생들의 수
+
+    print(f'{round(above_average / N * 100, 3):.3f}%')
